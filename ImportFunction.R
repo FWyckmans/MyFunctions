f2a3ce42909fba513beed9c0122381d68eca2b1a
FWyckmans <fwyckmans@gmail.com>
2021-01-23 20:40:39 +0100

    Prettier importer

--- ImportFunction.R
+++ ImportFunction.R
@@ -2,12 +2,21 @@
 ToImport <- "TestFunction"
 
 Importer <- function(ToImport, TypeFun = "Stat", FoldLocation = "C:/Users/fwyck/OneDrive/Documents/Project_DS/Project_R_Function/MyFunctions"){
-  if (TypeFun == "Stat"){
+
+## Type of functions  
+    if (TypeFun == "Stat"){
     Location <- paste0(FoldLocation, "/StatFunctions/")
   }
   
+  if (TypeFun == "Graph"){
+    Location <- paste0(FoldLocation, "/GraphFunctions/")
+  }
+
+## Actual importer  
   for (i in length(ToImport)){
     Import <- paste0(Location, "/", ToImport[i], ".R")
     source(Import)
   }
 }
+
+# FW
