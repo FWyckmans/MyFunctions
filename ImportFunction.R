d948a06528f8e114c5fb0db0246167080124b337
FWyckmans <fwyckmans@gmail.com>
2021-01-23 20:32:33 +0100

    Importer done

--- ImportFunction.R
+++ ImportFunction.R
@@ -1,10 +1,17 @@
 ##### Import personal functions
-LocationStat <- "~/Project_DS/Project_RFunction/MyFunctions/StatFunctions"
 
-dir()
+ToImport <- "TestFunction"
 
-ToImport <- c("") # Insert here the function to import
-
-for (i in length(ToImport)){
-  source(ToImport[i])
+Importer <- function(ToImport, TypeFun = "Stat", FoldLocation = "C:/Users/fwyck/OneDrive/Documents/Project_DS/Project_R_Function/MyFunctions"){
+  if (TypeFun == "Stat"){
+    Location <- paste0(FoldLocation, "/StatFunctions/")
+  }
+  
+  for (i in length(ToImport)){
+    Import <- paste0(Location, "/", ToImport[i], ".R")
+    source(Import)
+  }
 }
+
+Importer("TestFunction")
+Test()
