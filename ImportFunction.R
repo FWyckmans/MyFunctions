d8e36c4fc466eaedd28edaf5b12f17d7f58874ee
FWyckmans <fwyckmans@gmail.com>
2021-01-23 19:58:56 +0100

    Creation of the function importer

---
+++ ImportFunction.R
@@ -0,0 +1,10 @@
+##### Import personnal functions
+LocationStat <- "~/Project_DS/Project_RFunction/MyFunctions/StatFunctions"
+
+dir()
+
+ToImport <- c("") # Insert here the function to import
+
+for (i in length(ToImport)){
+  source(ToImport[i])
+}
