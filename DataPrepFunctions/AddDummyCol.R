2f36eba7b0a72cb52dc44b3e31223b52a94348a1
FWyckmans <fwyckmans@gmail.com>
2021-02-12 12:24:10 +0100

    Added a way to specify another value to insert in the new column (default = NA)

--- DataPrepFunctions/AddDummyCol.R
+++ DataPrepFunctions/AddDummyCol.R
@@ -1,7 +1,7 @@
-AddDummyCol <- function(d, ToAdd){
+AddDummyCol <- function(d, ToAdd, Val = NA){
   ##### Give the df you want to add dummy columns to and the column names #####
   for (i in ToAdd) {
-    Vect <- rep(NA, length(d[[1]]))
+    Vect <- rep(Val, length(d[[1]]))
     d <- cbind(d, Vect)
     colnames(d)[colnames(d) == "Vect"] <- i
   }
