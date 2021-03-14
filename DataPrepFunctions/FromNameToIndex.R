7f0fd1bbd9cf7482aa401f845bcc1cf8e090e86d
FWyckmans <fwyckmans@gmail.com>
2021-03-14 19:12:28 +0100

    Added a function to get col index from names

---
+++ DataPrepFunctions/FromNameToIndex.R
@@ -0,0 +1,12 @@
+FromNameToIndex <- function(d, CoI){
+  compt = 1
+  Indices <- c()
+  for (i in CoI){
+    coln <- paste0("\\b", i, "\\b")
+    index <- grep(coln, colnames(d))
+    
+    Indices[compt] <- index
+    compt = compt + 1
+  }
+  Indices
+}
