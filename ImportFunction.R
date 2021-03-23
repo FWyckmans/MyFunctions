a238ab7edcde79dbf00dd04b36263158f0e27556
FWyckmans <fwyckmans@gmail.com>
2021-03-23 13:07:54 +0100

    Misc

--- ImportFunction.R
+++ ImportFunction.R
@@ -2,9 +2,7 @@
 # Meta-function I copy paste in any other script to import functions in these folders
 # I update it each time I add a new functions folder
 
-# ToImport: Insert the name of the function (without .R).
-# TypeFun: Stat, Graph, DataPrep or Table at the moment.
-# FoldLocation: Default is my folder location, change it with yours.
+# FoldLocation: AllFun folder location.
 
 Importer <- function(FunctionPath = "AllFun/"){
   for (Fun in dir(FunctionPath)) {
