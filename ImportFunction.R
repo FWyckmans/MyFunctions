914dfb7ae2c2d091fbd943c8831bd4e45e4892b6
FWyckmans <fwyckmans@gmail.com>
2021-03-23 13:07:10 +0100

    Reorganized functions

--- ImportFunction.R
+++ ImportFunction.R
@@ -6,31 +6,9 @@
 # TypeFun: Stat, Graph, DataPrep or Table at the moment.
 # FoldLocation: Default is my folder location, change it with yours.
 
-
-Importer <- function(ToImport, TypeFun = "Stat", FoldLocation = "C:/Users/fwyck/OneDrive/Documents/Project_DS/Project_R_Function/MyFunctions"){
-
-## Type of functions  
-    if (TypeFun == "Stat"){
-    Location <- paste0(FoldLocation, "/StatFunctions/")
-  }
-  
-  if (TypeFun == "Graph"){
-    Location <- paste0(FoldLocation, "/GraphFunctions/")
-  }
-  
-  if (TypeFun == "Table"){
-    Location <- paste0(FoldLocation, "/TableFunctions/")
-  }
-  
-  if (TypeFun == "DataPrep"){
-    Location <- paste0(FoldLocation, "/DataPrepFunctions/")
-  }
-
-## Actual importer  
-  for (i in length(ToImport)){
-    Import <- paste0(Location, "/", ToImport[i], ".R")
-    source(Import)
+Importer <- function(FunctionPath = "AllFun/"){
+  for (Fun in dir(FunctionPath)) {
+    source(paste0(FunctionPath, Fun))
   }
 }
-
 # FW
