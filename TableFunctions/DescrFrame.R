75a38a997557f6a71dfdb6073e484adfdf0e61d1
FWyckmans <fwyckmans@gmail.com>
2021-03-23 12:58:49 +0100

    Updated DescrFrame

--- TableFunctions/DescrFrame.R
+++ TableFunctions/DescrFrame.R
@@ -41,6 +41,9 @@ DescrFrame <- function(d, Btwn = NA){
   
   # Compute n
   nPresAbs <- function(v, ...){
+    if (!is.factor(v)){
+      v <- as.factor(v)
+    }
     levelname <- levels(v)
     nl <- length(levelname)
     n = length(v[!is.na(v)])
@@ -54,12 +57,13 @@ DescrFrame <- function(d, Btwn = NA){
   
   ##### Specific function
   # Compute mean or n according to the type of subject
-  FinalFrame <- function(...){
+  FinalFrame <- function(d){
     Descriptive <- rep(NA, each = (length(d)-1))
     for (i in 1:(length(d)-1)) {
       if (is.factor(d[[i]])){
         Descriptive[i] <- nPresAbs(d[[i]])
       }
+      
       if (!is.factor(d[[i]])){
         Descriptive[i] <- MeanSD(d[[i]])
       }
@@ -67,8 +71,14 @@ DescrFrame <- function(d, Btwn = NA){
     return(Descriptive)
   }
   
-  
   ########## Main code
+  ##### Factorize character columns
+  for (i in 1:length(d)) {
+    if(is.character(d[[1,i]])){
+      d[[i]] <- as.factor(d[[i]])
+    }
+  }
+  
   ##### Get the index of the between subject column
   d <- AddDummyCol(d, "Dummy", "Every Subject")
   if (is.na(Btwn)){
@@ -84,13 +94,13 @@ DescrFrame <- function(d, Btwn = NA){
   colnames(dDescr)[1] <- "Variable"
   
   ##### Fill the descriptive frame by group
-  for (i in unique(levels(d[[Btwn]]))){
+  for (i in levels(d[[Btwn]])){
     dt <- filter(d, d[[Btwn]] == i)
     dT <- FinalFrame(dt)
     dDescr <- cbind(dDescr, dT)
     colnames(dDescr)[length(dDescr)] <- paste0(i,
                                                " (n = ",
-                                               length(d[[Btwn]][d$Hospit == i]),
+                                               length(d[[Btwn]][d[[Btwn]] == i]),
                                                ")")
   }
   return(dDescr)
